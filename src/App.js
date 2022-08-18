09e187812eb367d05c68de56a2867e55017ee312
carraa99 <carawalteji@gmail.com>
2022-08-18 10:20:01 +0300

    added some cool styles to the cards on the home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Experience from "./pages/Experience";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/experience" element={<Experience />}></Route>
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,98 @@
+import React from "react";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GithubIcon from "@material-ui/icons/GitHub";
+import Card from "react-bootstrap/Card";
+import { Container } from "react-bootstrap";
+import TelegramIcon from "@material-ui/icons/Telegram";
+import "../styles/Home.css";
+import frontend from "../assets/frontend.jpg";
+import backend from "../assets/backend.webp";
+import planguages from "../assets/planguages.webp";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="about">
+        <h2> Hi, My Name is Chera,</h2>
+        <br /> <br />
+        <div className="prompt">
+          <p>A software developer who loves learning and developing.</p>
+          <a href="https://linkedin.com/in/cara-walteji-6a700b197">
+            <LinkedInIcon />
+          </a>
+          <a href="https://t.me/Chance_stage">
+            <TelegramIcon />
+          </a>
+          <a href="https://github.com/carraa99/">
+            <GithubIcon />
+          </a>
+        </div>
+      </div>
+      <br /> <br />
+      <Container>
+        <div className="cards">
+          <Container>
+            <div className="card1">
+              <Card style={{ width: "18rem" }}>
+                <Card.Img className="card-img" variant="top" src={frontend} />
+
+                <Card.Body>
+                  <Card.Title>
+                    <h3>
+                      <b>FRONTEND</b>
+                    </h3>
+                  </Card.Title>
+                  <Card.Text>
+                    <p>
+                      HTML CSS ReactJS BootStrap MaterialUI React-Bootstrap
+                      Flutter
+                    </p>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+          </Container>
+          <Container>
+            <div className="card2">
+              <Card style={{ width: "18rem" }}>
+                <Card.Img className="card-img" variant="top" src={backend} />
+                <Card.Body>
+                  <Card.Title>
+                    <h3>
+                      <b>BACKEND</b>
+                    </h3>
+                  </Card.Title>
+                  <Card.Text>
+                    <p>NodeJS ExpressJS MySQL MongoDB PHP</p>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+          </Container>
+          <Container>
+            <div className="card3">
+              <Card style={{ width: "18rem" }}>
+                <Card.Img className="card-img" variant="top" src={planguages} />
+                <Card.Body>
+                  <br />
+                  <br />
+                  <br />
+                  <Card.Title>
+                    <h3>
+                      <b>LANGUAGES</b>
+                    </h3>
+                  </Card.Title>
+                  <Card.Text>
+                    <p>JavaScript Java Python C C++</p>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+          </Container>
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default Home;
